67674d6ba021ec5ac8682ab62f496de39db6d7de
Bahrom1dd1n <komiljonovbahromiddin@gmail.com>
2025-07-10 19:13:47 +0500

    Make some fixes

--- src/Snake.cpp
+++ src/Snake.cpp
@@ -1,17 +1,34 @@
 #include "Snake.h"
 
 #include <cstdio>
+
+#include "Game.h"
 void Snake::Move() {
 	Coord& prev_head = body[head];
 	head++;
 	if (head > sizeof(body)) head = 0;
 	Coord& current_head = body[head];
-	if (bit_map[current_head.y][current_head.x << 32] & 1) tale++;
+	if (game->GetCell(current_head.x, current_head.y)) {
+		Stop();
+		return;
+	}
+	game->SetCell(current_head.x, current_head.y, 1);
+	game->SetCell(body[tale].x, body[tale].y, 0);
+	tale++;
 	if (head > sizeof(body)) tale = 0;
+	game->SetCell(body[tale].x, body[tale].y, 1);
 	if (dx) body[head].x = prev_head.x + dx;
 	if (dy) body[head].y = prev_head.y + dy;
 }
-void Snake::Stop() {}
+void Snake::Stop() {
+	// this function is called when snake is dead
+	this->alive = 0;
+	for (int i = this->length, j = this->tale; i > -1 && j != head; i--, j++) {
+		printf("\033[%d;%dH%c", body[j].x, body[j].y, body_shape);
+		this->game->SetCell(body[j].x, body[j].y, 0);
+	}
+	// printf("\033[%d;%dH%c", body[head].x, body[head].y, head_shape);
+}
 void Snake::Draw() {
 	for (int i = this->length, j = this->tale; i > 0 && j != head; i--, j++) {
 		printf("\033[%d;%dH%c", body[j].x, body[j].y, body_shape);
